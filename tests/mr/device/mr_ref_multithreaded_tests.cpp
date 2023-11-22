e0aba2eb22abce8ca93fe011fccda1a7303bafb2
domcharrier <docharri@amd.com>
2023-11-22 17:28:55 +0000

    fix (mr_multithreadedtests.cpp): no 3rd arg default value with hipStreamWaitEvent

--- tests/mr/device/mr_ref_multithreaded_tests.cpp
+++ tests/mr/device/mr_ref_multithreaded_tests.cpp
@@ -14,6 +14,28 @@
  * limitations under the License.
  */
 
+// MIT License
+//
+// Modifications Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include "mr_ref_test.hpp"
 
 #include <rmm/cuda_stream.hpp>
@@ -255,7 +277,10 @@ void deallocate_async_loop(rmm::device_async_resource_ref ref,
   for (std::size_t i = 0; i < num_allocations; i++) {
     std::unique_lock lock(mtx);
     allocations_ready.wait(lock, [&allocations] { return !allocations.empty(); });
-    RMM_CUDA_TRY(cudaStreamWaitEvent(stream.value(), event));
+    //: NOTE(HIP/AMD): Different to cudaStreamWaitEvent(...), the third argument of
+    //:                hipStreamWaitEvent(...) does not default to 0 for older
+    //:                ROCm versions. We thus specify the CUDA default value (0) here manually.
+    RMM_CUDA_TRY(cudaStreamWaitEvent(stream.value(), event, 0));
     allocation alloc = allocations.front();
     allocations.pop_front();
     ref.deallocate_async(alloc.ptr, alloc.size, stream);
