CHIP-SPV/hipMM

commit 4e27f876315dd869a54ae8bc8ec74427c506f54d

Create logger wrapper around spdlog that can be easily reused in other libraries (#1722)
This PR defines a new way to produce a logger wrapping spdlog. The logger's interface is declared in a template header file that can be processed by CMake to produce an interface that may be customized for placement into any project. The new implementation uses the PImpl idiom to isolate the spdlog (and transitively, fmt) dependency from the public API of the logger. The implementation is defined in an impl header. A corresponding source template file is provided that simply includes this header. All of these files are wrapped in some CMake logic for producing a custom target for a given project.

rmm leverages this new logger by requesting the creation of a logger target and a corresponding implementation. This is a breaking change because consumers of rmm will need to link the new `rmm_logger_impl` target into their own libraries to get logging support. Once this gets merged, the plan is to move this implementation out of rmm into its own repository. At that point, the logger may also be used to completely replace logger implementations in cudf, raft, and cuml (as well as any other RAPIDS libraries that are aiming to provide their own logging implementation). Once everything in RAPIDS is migrated to using the new logger, we will update the way that it uses spdlog to completely hide all spdlog symbols, which solves a half dozen different problems for us when it comes to packaging (symbol collision issues, ABI compatibility, conda environment conflicts, bundling of headers into conda packages, etc).

Resolves #1709

Authors:
  - Vyas Ramasubramani (https://github.com/vyasr)

Approvers:
  - Robert Maynard (https://github.com/robertmaynard)
  - Mark Harris (https://github.com/harrism)
  - Jake Awe (https://github.com/AyodeAwe)

URL: https://github.com/rapidsai/rmm/pull/1722

CMakeLists.txt
@@ -73,7 +73,9 @@ rapids_find_package(
 # add third party dependencies using CPM
 rapids_cpm_init()
 
-include(cmake/thirdparty/get_spdlog.cmake)
+add_subdirectory(rapids_logger)
+rapids_make_logger(rmm EXPORT_SET rmm-exports)
+
 include(cmake/thirdparty/get_cccl.cmake)
 include(cmake/thirdparty/get_nvtx.cmake)
 
@@ -93,8 +95,8 @@ else()
   target_link_libraries(rmm INTERFACE CUDA::cudart)
 endif()
 
+target_link_libraries(rmm INTERFACE rmm_logger)
 target_link_libraries(rmm INTERFACE CCCL::CCCL)
-target_link_libraries(rmm INTERFACE spdlog::spdlog_header_only)
 target_link_libraries(rmm INTERFACE dl)
 target_link_libraries(rmm INTERFACE nvtx3::nvtx3-cpp)
 target_compile_features(rmm INTERFACE cxx_std_17 $<BUILD_INTERFACE:cuda_std_17>)

ci/check_symbols.sh
@@ -47,32 +47,13 @@ for dso_file in ${dso_files}; do
     echo "  * WEAK: $(grep --count -E '  WEAK '    < ${symbol_file})"
     echo "  * LOCAL: $(grep --count -E '  LOCAL '  < ${symbol_file})"
 
-    # Explanation for '-v' uses here:
-    #
-    #   * 'format_error' symbols are intentionally exported, that type of error
-    #      can be thrown across library boundaries. See "Problems with C++ exceptions"
-    #      at https://gcc.gnu.org/wiki/Visibility.
     echo "checking for 'fmt::' symbols..."
-    if grep -E 'fmt\:\:' < "${symbol_file}" \
-        | grep -v 'format_error'
-    then
+    if grep -E 'fmt\:\:' < "${symbol_file}"; then
         raise-symbols-found-error 'fmt::'
     fi
 
-    # Explanation for '-v' uses here:
-    #
-    #  * trivially-destructible objects sometimes get an entry in the symbol table
-    #    for a specialization of `std::_Destroy_aux()` called to destroy them.
-    #    There is one for `spdlog::details::log_msg_buffer like that:
-    #
-    #       'std::_Destroy_aux<false>::__destroy<spdlog::details::log_msg_buffer*>'
-    #
-    #    That should be safe to export.
-    #
     echo "checking for 'spdlog::' symbols..."
-    if grep -E 'spdlog\:\:' < "${symbol_file}" \
-        | grep -v 'std\:\:_Destroy_aux'
-    then
+    if grep -E 'spdlog\:\:' < "${symbol_file}"; then
         raise-symbols-found-error 'spdlog::'
     fi
     echo "No symbol visibility issues found"

cmake/thirdparty/get_spdlog.cmake
@@ -12,7 +12,7 @@
 # the License.
 # =============================================================================
 
-# Use CPM to find or clone speedlog
+# Use CPM to find or clone speedlog.
 function(find_and_configure_spdlog)
 
   include(${rapids-cmake-dir}/cpm/spdlog.cmake)
@@ -20,7 +20,6 @@ function(find_and_configure_spdlog)
     FMT_OPTION "EXTERNAL_FMT_HO"
     INSTALL_EXPORT_SET rmm-exports
     BUILD_EXPORT_SET rmm-exports)
-
 endfunction()
 
 find_and_configure_spdlog()

include/rmm/detail/logging_assert.hpp
@@ -31,7 +31,7 @@
  */
 #ifdef NDEBUG
 #define RMM_LOGGING_ASSERT(_expr) (void)0
-#elif SPDLOG_ACTIVE_LEVEL < SPDLOG_LEVEL_OFF
+#elif RMM_LOG_ACTIVE_LEVEL < RMM_LOG_LEVEL_OFF
 #define RMM_LOGGING_ASSERT(_expr)                                                                 \
   do {                                                                                            \
     bool const success = (_expr);                                                                 \

rapids_logger/logger_impl.hpp.in
@@ -1,0 +1,205 @@
+/*
+ * Copyright (c) 2024, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "../logger.hpp"
+
+// Include C headers for which we need symbols to be made public and don't want
+// the below symbol hiding pattern to apply.
+#include <sys/stat.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+
+// Start hiding before including spdlog headers.
+#pragma GCC visibility push(hidden)
+
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/ostream_sink.h>
+#include <spdlog/spdlog.h>
+
+#include <memory>
+#include <sstream>
+#include <string>
+
+namespace @_RAPIDS_LOGGER_NAMESPACE@ {
+
+namespace detail {
+namespace {
+
+/**
+ * @brief Convert a string to a log level.
+ *
+ * This function is used to process env-var specifications of log levels.
+ * @param env_lvl_str The string to convert.
+ * @return The log level.
+ */
+level_enum string_to_level(std::string_view const env_lvl_str)
+{
+    if (env_lvl_str == "TRACE") return level_enum::trace;
+    if (env_lvl_str == "DEBUG") return level_enum::debug;
+    if (env_lvl_str == "INFO") return level_enum::info;
+    if (env_lvl_str == "WARN") return level_enum::warn;
+    if (env_lvl_str == "ERROR") return level_enum::error;
+    if (env_lvl_str == "CRITICAL") return level_enum::critical;
+    if (env_lvl_str == "OFF") return level_enum::off;
+    std::ostringstream os{};
+    os << "Invalid logging level: " << env_lvl_str;
+    throw std::invalid_argument(os.str());
+}
+
+/**
+ * @brief Convert a log level to an spdlog log level.
+ *
+ * @param lvl The log level to convert.
+ * @return The spdlog log level.
+ */
+spdlog::level::level_enum to_spdlog_level(level_enum lvl)
+{
+  return static_cast<spdlog::level::level_enum>(static_cast<int32_t>(lvl));
+}
+
+/**
+ * @brief Convert an spdlog log level to a log level.
+ *
+ * @param lvl The spdlog log level to convert.
+ * @return The log level.
+ */
+level_enum from_spdlog_level(spdlog::level::level_enum lvl)
+{
+  return static_cast<level_enum>(static_cast<int32_t>(lvl));
+}
+}
+
+/**
+ * @brief The sink_impl class is a wrapper around an spdlog sink.
+ *
+ * This class is the impl part of the PImpl for the sink.
+ */
+class sink_impl {
+public:
+  sink_impl(std::shared_ptr<spdlog::sinks::sink> sink) : underlying{sink} {}
+private:
+  std::shared_ptr<spdlog::sinks::sink> underlying;
+  // The sink_vector needs to be able to pass the underlying sink to the spdlog logger.
+  friend class logger::sink_vector;
+};
+
+/**
+ * @brief The logger_impl class is a wrapper around an spdlog logger.
+ *
+ * This class is the impl part of the PImpl for the logger.
+ */
+class logger_impl {
+ public:
+  logger_impl(std::string name) : underlying{spdlog::logger{name}} {
+    underlying.set_pattern("[%6t][%H:%M:%S:%f][%-6l] %v");
+    auto const env_logging_level =
+      std::getenv("@_RAPIDS_LOGGER_MACRO_PREFIX@_DEFAULT_LOGGING_LEVEL");
+    if (env_logging_level != nullptr) { set_level(detail::string_to_level(env_logging_level)); }
+    auto const env_flush_level = std::getenv("@_RAPIDS_LOGGER_MACRO_PREFIX@_DEFAULT_FLUSH_LEVEL");
+    if (env_flush_level != nullptr) { flush_on(detail::string_to_level(env_flush_level)); }
+  }
+
+  void log(level_enum lvl, std::string const& message) { underlying.log(to_spdlog_level(lvl), message); }
+  void set_level(level_enum log_level) { underlying.set_level(to_spdlog_level(log_level)); }
+  void flush() { underlying.flush(); }
+  void flush_on(level_enum log_level) { underlying.flush_on(to_spdlog_level(log_level)); }
+  level_enum flush_level() const { return from_spdlog_level(underlying.flush_level()); }
+  bool should_log(level_enum lvl) const { return underlying.should_log(to_spdlog_level(lvl)); }
+  level_enum level() const { return from_spdlog_level(underlying.level()); }
+  void set_pattern(std::string pattern) { underlying.set_pattern(pattern); }
+  const std::vector<spdlog::sink_ptr> &sinks() const { return underlying.sinks(); }
+  std::vector<spdlog::sink_ptr> &sinks() { return underlying.sinks(); }
+
+private:
+  spdlog::logger underlying;  ///< The spdlog logger
+};
+
+}  // namespace detail
+
+// Sink vector functions
+void logger::sink_vector::push_back(sink_ptr const& sink) {
+  sinks_.push_back(sink);
+  parent.impl->sinks().push_back(sink->impl->underlying);
+}
+void logger::sink_vector::push_back(sink_ptr&& sink) {
+  sinks_.push_back(sink);
+  parent.impl->sinks().push_back(sink->impl->underlying);
+}
+void logger::sink_vector::pop_back() {
+  sinks_.pop_back();
+  parent.impl->sinks().pop_back();
+}
+void logger::sink_vector::clear() {
+  sinks_.clear();
+  parent.impl->sinks().clear();
+}
+
+// Sink methods
+sink::sink(std::unique_ptr<detail::sink_impl> impl) : impl{std::move(impl)} {}
+
+sink::~sink() = default;
+
+basic_file_sink_mt::basic_file_sink_mt(std::string const& filename, bool truncate)
+  : sink{std::make_unique<detail::sink_impl>(std::make_shared<spdlog::sinks::basic_file_sink_mt>(filename, truncate))} {}
+
+ostream_sink_mt::ostream_sink_mt(std::ostream& stream, bool force_flush)
+  : sink{std::make_unique<detail::sink_impl>(std::make_shared<spdlog::sinks::ostream_sink_mt>(stream, force_flush))} {}
+
+// Logger methods
+logger::logger(std::string name, std::string filename)
+  : impl{std::make_unique<detail::logger_impl>(name)}, sinks_{*this} {
+  sinks_.push_back(std::make_shared<basic_file_sink_mt>(filename, true));
+}
+
+logger::logger(std::string name, std::ostream& stream)
+  : impl{std::make_unique<detail::logger_impl>(name)}, sinks_{*this} {
+  sinks_.push_back(std::make_shared<ostream_sink_mt>(stream));
+}
+
+logger::logger(std::string name, std::vector<sink_ptr> sinks)
+  : impl{std::make_unique<detail::logger_impl>(name)}, sinks_{*this} {
+  for (auto const& s : sinks) {
+    sinks_.push_back(s);
+  }
+}
+
+logger::~logger() = default;
+logger::logger(logger&& other) = default;
+logger& logger::operator=(logger&& other) {
+  impl = std::move(other.impl);
+  sinks_.clear();
+  for (auto const& s : other.sinks_) {
+    sinks_.push_back(s);
+  }
+  return *this;
+}
+
+void logger::log(level_enum lvl, std::string const& message) { impl->log(lvl, message); }
+void logger::set_level(level_enum log_level) { impl->set_level(log_level); }
+void logger::flush() { impl->flush(); }
+void logger::flush_on(level_enum log_level) { impl->flush_on(log_level); }
+level_enum logger::flush_level() const { return impl->flush_level(); }
+bool logger::should_log(level_enum lvl) const { return impl->should_log(lvl); }
+level_enum logger::level() const { return impl->level(); }
+void logger::set_pattern(std::string pattern) { impl->set_pattern(pattern); }
+const logger::sink_vector& logger::sinks() const { return sinks_; }
+logger::sink_vector& logger::sinks() { return sinks_; }
+
+}  // namespace @_RAPIDS_LOGGER_NAMESPACE@
+// This visibility pragma must be here so that both our logger types and those coming from includes are hidden.
+#pragma GCC visibility pop

benchmarks/CMakeLists.txt
@@ -34,9 +34,9 @@ function(ConfigureBench BENCH_NAME)
                RUNTIME_OUTPUT_DIRECTORY "$<BUILD_INTERFACE:${RMM_BINARY_DIR}/gbenchmarks>"
                CUDA_ARCHITECTURES "${CMAKE_CUDA_ARCHITECTURES}"
                INSTALL_RPATH "\$ORIGIN/../../../lib")
-  target_link_libraries(${BENCH_NAME} benchmark::benchmark pthread rmm)
+  target_link_libraries(${BENCH_NAME} PRIVATE benchmark::benchmark pthread rmm)
   target_compile_definitions(${BENCH_NAME}
-                             PUBLIC "SPDLOG_ACTIVE_LEVEL=SPDLOG_LEVEL_${RMM_LOGGING_LEVEL}")
+                             PUBLIC "RMM_LOG_ACTIVE_LEVEL=RMM_LOG_LEVEL_${RMM_LOGGING_LEVEL}")
 
   if(PER_THREAD_DEFAULT_STREAM)
     target_compile_definitions(${BENCH_NAME} PUBLIC CUDA_API_PER_THREAD_DEFAULT_STREAM)
@@ -44,6 +44,9 @@ function(ConfigureBench BENCH_NAME)
 
   target_compile_options(${BENCH_NAME} PUBLIC $<$<COMPILE_LANG_AND_ID:CXX,GNU,Clang>:-Wall -Werror
                                               -Wno-unknown-pragmas>)
+
+  target_link_libraries(${BENCH_NAME} PRIVATE rmm_bench_logger)
+
   if(DISABLE_DEPRECATION_WARNING)
     target_compile_options(
       ${BENCH_NAME} PUBLIC $<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler=-Wno-deprecated-declarations>)
@@ -57,6 +60,10 @@ function(ConfigureBench BENCH_NAME)
     DESTINATION bin/benchmarks/librmm
     EXCLUDE_FROM_ALL)
 endfunction(ConfigureBench)
+
+# Create an object library for the logger so that we don't have to recompile it.
+add_library(rmm_bench_logger OBJECT)
+target_link_libraries(rmm_bench_logger PRIVATE rmm_logger_impl)
 
 # random allocations benchmark
 ConfigureBench(RANDOM_ALLOCATIONS_BENCH random_allocations/random_allocations.cpp)

python/rmm/CMakeLists.txt
@@ -27,7 +27,11 @@ include(rapids-cython-core)
 rapids_cython_init()
 
 # pass through logging level to spdlog
-add_compile_definitions("SPDLOG_ACTIVE_LEVEL=SPDLOG_LEVEL_${RMM_LOGGING_LEVEL}")
+add_compile_definitions("RMM_LOG_ACTIVE_LEVEL=RMM_LOG_LEVEL_${RMM_LOGGING_LEVEL}")
+
+# Create an object library for the logger so that we don't have to recompile it.
+add_library(cpp_logger OBJECT)
+target_link_libraries(cpp_logger PRIVATE rmm::rmm_logger_impl)
 
 add_subdirectory(rmm/_cuda)
 add_subdirectory(rmm/librmm)

python/rmm/rmm/_cuda/CMakeLists.txt
@@ -13,7 +13,7 @@
 # =============================================================================
 
 set(cython_sources stream.pyx)
-set(linked_libraries rmm::rmm)
+set(linked_libraries rmm::rmm cpp_logger)
 
 rapids_cython_create_modules(SOURCE_FILES "${cython_sources}" LINKED_LIBRARIES "${linked_libraries}"
                                                                                CXX)

commit 7b6a68904ed521fb9454bc5d2b288cf63d90070d

Fix some logger issues (#1739)
This fixes a handful of issues uncovered in downstream CI after #1722.

The following are bugs introduced in #1722:
- When using rmm from the build directory rather than an installation, the namespaced targets are not present so we must generate aliases.
- The `RMM_LOGGING_ASSERT` macro is never used in rmm itself, so we didn't catch that it was still using the old version of the logger.

While fixing the above, I also uncovered that building fmt in this environment unearths a gcc bug.

The following are underlying issues uncovered by #1722:
- spdlog's fmt CMake linkage is determined at build time. As a result, the conda package for spdlog is hardcoded to use fmt as a library (static or shared depends on what the `fmt::fmt` target winds up being when a consumer using spdlog finds fmt in CMake), which means that is propagated to all consumers of the librmm package via its CMake. This means that we often wind up with both fmt_header_only and fmt as link targets for many RAPIDS libraries. For now, this PR makes it so that if `rapids_cpm_find(spdlog)` does not find a copy of spdlog locally, the cloned version will use an external header-only fmt via rapids-cmake's logic, which ensures that packages like wheels do not export a libfmt or libspdlog dependency. However, in environments where `rapids_cpm_find(spdlog)` does find a preexisting package, we allow that package's fmt linkage to propagate. In conda environments, we know that this fmt linkage is to the library, so we keep fmt as part of rmm's runtime dependencies (by placing it in host and relying on the run export) so that libfmt is always available in environments using rmm.

Authors:
  - Vyas Ramasubramani (https://github.com/vyasr)

Approvers:
  - Bradley Dice (https://github.com/bdice)

URL: https://github.com/rapidsai/rmm/pull/1739

CMakeLists.txt
@@ -75,6 +75,8 @@ rapids_cpm_init()
 
 add_subdirectory(rapids_logger)
 rapids_make_logger(rmm EXPORT_SET rmm-exports)
+add_library(rmm::rmm_logger ALIAS rmm_logger)
+add_library(rmm::rmm_logger_impl ALIAS rmm_logger_impl)
 
 include(cmake/thirdparty/get_cccl.cmake)
 include(cmake/thirdparty/get_nvtx.cmake)

ci/check_symbols.sh
@@ -53,7 +53,9 @@ for dso_file in ${dso_files}; do
     fi
 
     echo "checking for 'spdlog::' symbols..."
-    if grep -E 'spdlog\:\:' < "${symbol_file}"; then
+    if grep -E 'spdlog\:\:' < "${symbol_file}" \
+        | grep -v 'std\:\:_Destroy_aux'
+    then
         raise-symbols-found-error 'spdlog::'
     fi
     echo "No symbol visibility issues found"

cmake/thirdparty/get_spdlog.cmake
@@ -17,6 +17,14 @@ function(find_and_configure_spdlog)
 
   include(${rapids-cmake-dir}/cpm/spdlog.cmake)
   rapids_cpm_spdlog(
+    # The conda package for fmt is hard-coded to assume that we use a preexisting fmt library. This
+    # is why we have always had a libfmt linkage despite choosing to specify the header-only version
+    # of fmt. We need a more robust way of modifying this to support fully self-contained build and
+    # usage even in environments where fmt and/or spdlog are already present. The crudest solution
+    # would be to modify the interface compile definitions and link libraries of the spdlog target,
+    # if necessary. For now I'm specifying EXTERNAL_FMT_HO here so that in environments where spdlog
+    # is cloned and built from source we wind up with the behavior that we expect, but we'll have to
+    # resolve this properly eventually.
     FMT_OPTION "EXTERNAL_FMT_HO"
     INSTALL_EXPORT_SET rmm-exports
     BUILD_EXPORT_SET rmm-exports)

conda/environments/all_cuda-118_arch-x86_64.yaml
@@ -35,7 +35,6 @@ dependencies:
 - python>=3.10,<3.13
 - rapids-build-backend>=0.3.0,<0.4.0.dev0
 - scikit-build-core >=0.10.0
-- spdlog>=1.14.1,<1.15
 - sphinx
 - sphinx-copybutton
 - sphinx-markdown-tables

conda/environments/all_cuda-125_arch-x86_64.yaml
@@ -34,7 +34,6 @@ dependencies:
 - python>=3.10,<3.13
 - rapids-build-backend>=0.3.0,<0.4.0.dev0
 - scikit-build-core >=0.10.0
-- spdlog>=1.14.1,<1.15
 - sphinx
 - sphinx-copybutton
 - sphinx-markdown-tables

conda/recipes/librmm/meta.yaml
@@ -26,13 +26,13 @@ requirements:
     - {{ stdlib("c") }}
   host:
     - cuda-version ={{ cuda_version }}
-    # We require spdlog and fmt (which was de-vendored from spdlog
-    # conda-forge packages in 1.11.0) so that the spdlog headers are not
-    # pulled by CPM and installed as a part of the rmm packages. However,
-    # building against librmm still requires these headers. They are also
-    # added as a run requirement via the packages' run_exports.
+    # We need fmt here for now because the conda spdlog package is hard-coded
+    # to use fmt as a compiled library, not header-only, so we must ensure that
+    # the library is present for now so that if a downstream library tries to
+    # build against rmm and some other package in its build environment uses
+    # fmt (or spdlog) that the default rmm build is consistent with such
+    # environments.
     - fmt {{ fmt_version }}
-    - spdlog {{ spdlog_version }}
 
 build:
   script_env:
@@ -77,8 +77,8 @@ outputs:
         {% if cuda_major == "11" %}
         - cudatoolkit
         {% endif %}
+        # See comment about fmt in the build section above.
         - fmt {{ fmt_version }}
-        - spdlog {{ spdlog_version }}
     test:
       commands:
         - test -d "${PREFIX}/include/rmm"

dependencies.yaml
@@ -112,7 +112,6 @@ dependencies:
           - c-compiler
           - cxx-compiler
           - fmt>=11.0.2,<12
-          - spdlog>=1.14.1,<1.15
     specific:
       - output_types: conda
         matrices:

include/rmm/detail/logging_assert.hpp
@@ -38,7 +38,7 @@
     if (!success) {                                                                               \
       RMM_LOG_CRITICAL(                                                                           \
         "[" __FILE__ ":" RMM_STRINGIFY(__LINE__) "] Assertion " RMM_STRINGIFY(_expr) " failed."); \
-      rmm::detail::logger().flush();                                                              \
+      rmm::default_logger().flush();                                                              \
       /* NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-array-to-pointer-decay) */                   \
       assert(success);                                                                            \
     }                                                                                             \

rapids_logger/logger_impl.hpp.in
@@ -26,10 +26,16 @@
 
 // Start hiding before including spdlog headers.
 #pragma GCC visibility push(hidden)
+// This issue claims to have been resolved in gcc 8, but we still seem to encounter it here.
+// The code compiles and links and all tests pass, and nm shows symbols resolved as expected.
+// https://gcc.gnu.org/bugzilla/show_bug.cgi?id=80947
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wattributes"
 
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/sinks/ostream_sink.h>
 #include <spdlog/spdlog.h>
+#pragma GCC diagnostic pop
 
 #include <memory>
 #include <sstream>