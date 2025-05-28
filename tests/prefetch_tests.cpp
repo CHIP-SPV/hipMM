0a38f8877fc7a7fa4bfdbe763008fa378e77ca56
domcharrier <docharri@amd.com>
2025-05-28 12:43:07 +0000

    hipify(tests/prefetch_tests.cpp)

--- tests/prefetch_tests.cpp
+++ tests/prefetch_tests.cpp
@@ -13,8 +13,30 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+// MIT License
+//
+// Modifications Copyright (C) 2025 Advanced Micro Devices, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
-#include "driver_types.h"
+// NOTE(HIP/AMD): This cuda include is not needed
+// #include "driver_types.h"
 #include "rmm/cuda_device.hpp"
 
 #include <rmm/cuda_stream.hpp>
@@ -59,7 +81,7 @@ struct PrefetchTest : public ::testing::Test {
       RMM_CUDA_TRY(
         cudaMemRangeGetAttribute(&prefetch_location,
                                  prefetch_data_size,
-                                 cudaMemRangeAttribute::cudaMemRangeAttributeLastPrefetchLocation,
+                                 hipMemRangeAttribute::hipMemRangeAttributeLastPrefetchLocation,
                                  ptr,
                                  size));
       EXPECT_EQ(prefetch_location, device.value());
